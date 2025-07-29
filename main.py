8e29d486a4a2977e85d4cc3713aa1296c687509c
essita <essita@noreply.gitcode.com>
2025-07-29 11:29:02 +0800

    适配windows

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import time
 import psutil
 import requests
+import win32com.client
 
 # 检查QQ是否正在运行
-def is_qq_running():
-    for proc in psutil.process_iter(['pid', 'name']):
-        print(f"{proc}")
-        if 'qq' in proc.info['name'].lower():
-            return True
-        else:
-            return False    
+# def is_qq_running():
+#     for proc in psutil.process_iter(['pid', 'name']):
+#         print(f"{proc}")
+#         if 'qq' in proc.info['name'].lower():
+#             return True
+#         else:
+#             return False    
 
 # 发送企业微信消息
 def send_wechat_message(webhook_url, message):
@@ -25,16 +26,35 @@ def send_wechat_message(webhook_url, message):
     print("done")
     return response.status_code
 
-# 主循环
-def monitor_qq(webhook_url, check_interval=1):
+# # 主循环
+# def monitor_qq(webhook_url, check_interval=1):
+#     while True:
+#         if not is_qq_running():
+#             # send_wechat_message(webhook_url, "QQ进程已停止，请检查！")
+#             print("test")
+#         time.sleep(check_interval)  # 每隔一段时间检查一次
+
+# # 配置企业微信机器人的Webhook地址
+webhook_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=67e62638-af00-4879-a146-b468fd718a30"
+
+# # 开始监控
+# monitor_qq(webhook_url)
+
+
+
+def is_qq_running():
+    wmi = win32com.client.GetObject("winmgmts:")
+    processes = wmi.ExecQuery("Select * from Win32_Process where Name like 'QQ.exe'")
+    print(processes)
+    return len(processes) > 0
+
+
+
+if __name__ == "__main__":
+    check_interval = 1  # 检查间隔时间（秒）
     while True:
         if not is_qq_running():
             send_wechat_message(webhook_url, "QQ进程已停止，请检查！")
         time.sleep(check_interval)  # 每隔一段时间检查一次
 
-# 配置企业微信机器人的Webhook地址
-webhook_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=67e62638-af00-4879-a146-b468fd718a30"
-
-# 开始监控
-monitor_qq(webhook_url)
 
